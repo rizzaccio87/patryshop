972287c01c57ffdc02adc10550e8298cb2402253
Alberto Rizzi <albe.rizzles@gmail.com>
2020-12-09 16:22:50 +0100

    Added first application entry point files

---
+++ src/main/java/com/projects/patryshop/PatryshopApplication.java
@@ -0,0 +1,13 @@
+package com.projects.patryshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatryshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatryshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/patryshop/PatryshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.patryshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatryshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
