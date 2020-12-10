6e91852f4644c10f9e2b23250ccd163e495a89d3
Alberto Rizzi <alberto.rizzi@siav.it>
2020-12-10 11:20:22 +0100

    Added exceptions.

---
+++ src/main/java/com/projects/patryshop/exceptions/EntityNotFoundException.java
@@ -0,0 +1,10 @@
+package com.projects.patryshop.exceptions;
+
+public class EntityNotFoundException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
